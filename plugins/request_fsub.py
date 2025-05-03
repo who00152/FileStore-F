e2770a3c8df84024166d4fb175f3bc09ef72671c
whoami847 <mrbeastbangla069@gmail.com>
2025-05-03 10:34:15 +0600

    Update request_fsub.py

--- plugins/request_fsub.py
+++ plugins/request_fsub.py
@@ -185,7 +185,7 @@ async def del_force_sub(client: Client, message: Message):
     all_channels = await db.show_channels()
 
     if len(args) != 2:
-        return await temp.edit("<b>Usage:</b> <code>/delchnl <channel_id | all></code>")
+        await temp.edit("<b>Usage:</b> <code>/delchnl &lt;channel_id | all&gt;</code>")
 
     if args[1].lower() == "all":
         if not all_channels:
