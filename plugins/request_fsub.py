faa5335083a20443c79981769de7e08afe339cb9
whoami847 <mrbeastbangla069@gmail.com>
2025-05-03 11:05:41 +0600

    Update request_fsub.py

--- plugins/request_fsub.py
+++ plugins/request_fsub.py
@@ -8,7 +8,7 @@
 # Please see < https://github.com/Codeflix-Bots/FileStore/blob/master/LICENSE >
 #
 # All rights reserved.
-#
+
 import asyncio
 import os
 import random
@@ -23,19 +23,6 @@ from config import *
 from helper_func import *
 from database.database import *
 
-# Don't Remove Credit @CodeFlix_Bots, @rohit_1888
-# Ask Doubt on telegram @CodeflixSupport
-#
-# Copyright (C) 2025 by Codeflix-Bots@Github, < https://github.com/Codeflix-Bots >.
-#
-# This file is part of < https://github.com/Codeflix-Bots/FileStore > project,
-# and is released under the MIT License.
-# Please see < https://github.com/Codeflix-Bots/FileStore/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-
-#Request force sub mode commad,,,,,,
 @Bot.on_message(filters.command('fsub_mode') & filters.private & admin)
 async def change_force_sub_mode(client: Client, message: Message):
     temp = await message.reply("<b><i>ᴡᴀɪᴛ ᴀ sᴇᴄ..</i></b>", quote=True)
@@ -63,7 +50,6 @@ async def change_force_sub_mode(client: Client, message: Message):
         disable_web_page_preview=True
     )
 
-# This handler captures membership updates (like when a user leaves, banned)
 @Bot.on_chat_member_updated()
 async def handle_Chatmembers(client, chat_member_updated: ChatMemberUpdated):    
     chat_id = chat_member_updated.chat.id
@@ -80,46 +66,25 @@ async def handle_Chatmembers(client, chat_member_updated: ChatMemberUpdated):
             if await db.req_user_exist(chat_id, user_id):
                 await db.del_req_user(chat_id, user_id)
 
-
-# This handler will capture any join request to the channel/group where the bot is an admin
 @Bot.on_chat_join_request()
 async def handle_join_request(client, chat_join_request):
     chat_id = chat_join_request.chat.id
     user_id = chat_join_request.from_user.id
 
-    #print(f"[JOIN REQUEST] User {user_id} sent join request to {chat_id}")
-
-    # Print the result of db.reqChannel_exist to check if the channel exists
-    channel_exists = await db.reqChannel_exist(chat_id)
-    #print(f"Channel {chat_id} exists in the database: {channel_exists}")
-
-    if channel_exists:
+    if await db.reqChannel_exist(chat_id):
         if not await db.req_user_exist(chat_id, user_id):
             await db.req_user(chat_id, user_id)
-            #print(f"Added user {user_id} to request list for {chat_id}")
 
-# Don't Remove Credit @CodeFlix_Bots, @rohit_1888
-# Ask Doubt on telegram @CodeflixSupport
-#
-# Copyright (C) 2025 by Codeflix-Bots@Github, < https://github.com/Codeflix-Bots >.
-#
-# This file is part of < https://github.com/Codeflix-Bots/FileStore > project,
-# and is released under the MIT License.
-# Please see < https://github.com/Codeflix-Bots/FileStore/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-
-# Add channel
 @Bot.on_message(filters.command('addchnl') & filters.private & admin)
 async def add_force_sub(client: Client, message: Message):
     temp = await message.reply("<b><i>ᴡᴀɪᴛ ᴀ sᴇᴄ..</i></b>", quote=True)
     args = message.text.split(maxsplit=1)
 
     if len(args) != 2:
+        buttons = [[InlineKeyboardButton("ᴄʟᴏsᴇ ✖️", callback_data="close")]]
         return await temp.edit(
-            "<b>ᴜsᴀɢᴇ:</b> <code>/addchnl -100XXXXXXXXXX</code>\n<b>ᴀᴅᴅ ᴏɴʟʏ ᴏɴᴇ ᴄʜᴀɴɴᴇʟ ᴀᴛ ᴀ ᴛɪᴍᴇ.</b>",
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("ᴄʟᴏsᴇ ✖️", callback_data="close")]])
+            "<b>ᴜꜱᴀɢᴇ:</b> <code>/addchnl -100XXXXXXXXXX</code>\n<b>ᴀᴅᴅ ᴏɴʟʏ ᴏɴᴇ ᴄʜᴀɴɴᴇʟ ᴀᴛ ᴀ ᴛɪᴍᴇ.</b>",
+            reply_markup=InlineKeyboardMarkup(buttons)
         )
 
     try:
@@ -139,73 +104,57 @@ async def add_force_sub(client: Client, message: Message):
             return await temp.edit("<b>❌ ᴏɴʟʏ ᴘᴜʙʟɪᴄ ᴏʀ ᴘʀɪᴠᴀᴛᴇ ᴄʜᴀɴɴᴇʟs ᴀʀᴇ ᴀʟʟᴏᴡᴇᴅ.</b>")
 
         member = await client.get_chat_member(chat.id, "me")
-        print(f"Bot status: {member.status} in chat: {chat.title} ({chat.id})")  # Debug
-
-        # FIXED ENUM COMPARISON
         if member.status not in [ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.OWNER]:
             return await temp.edit("<b>❌ ʙᴏᴛ ᴍᴜsᴛ ʙᴇ ᴀɴ ᴀᴅᴍɪɴ ɪɴ ᴛʜᴀᴛ ᴄʜᴀɴɴᴇʟ.</b>")
 
-        # Get invite link
-        try:
-            link = await client.export_chat_invite_link(chat.id)
-        except Exception:
-            link = f"https://t.me/{chat.username}" if chat.username else f"https://t.me/c/{str(chat.id)[4:]}"
-
+        link = await client.export_chat_invite_link(chat.id) if not chat.username else f"https://t.me/{chat.username}"
+        
         await db.add_channel(channel_id)
         return await temp.edit(
-            f"<b>✅ Forceғᴏʀᴄᴇ-sᴜʙ ᴄʜᴀɴɴᴇʟ ᴀᴅᴅᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ!</b>\n\n"
+            f"<b>✅ ғᴏʀᴄᴇ-sᴜʙ ᴄʜᴀɴɴᴇʟ ᴀᴅᴅᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ!</b>\n\n"
             f"<b>ɴᴀᴍᴇ:</b> <a href='{link}'>{chat.title}</a>\n"
             f"<b>ɪᴅ:</b> <code>{channel_id}</code>",
             disable_web_page_preview=True
         )
 
     except Exception as e:
-        return await temp.edit(
-            f"<b>❌ ғᴀɪʟᴇᴅ ᴛᴏ ᴀᴅᴅ ᴄʜᴀɴɴᴇʟ:</b>\n<code>{channel_id}</code>\n\n<i>{e}</i>"
-        )
+        return await temp.edit(f"<b>❌ ғᴀɪʟᴇᴅ ᴛᴏ ᴀᴅᴅ ᴄʜᴀɴɴᴇʟ:</b>\n<code>{channel_id}</code>\n\n<i>{e}</i>")
 
-
-# Don't Remove Credit @CodeFlix_Bots, @rohit_1888
-# Ask Doubt on telegram @CodeflixSupport
-#
-# Copyright (C) 2025 by Codeflix-Bots@Github, < https://github.com/Codeflix-Bots >.
-#
-# This file is part of < https://github.com/Codeflix-Bots/FileStore > project,
-# and is released under the MIT License.
-# Please see < https://github.com/Codeflix-Bots/FileStore/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-
-# Delete channel
 @Bot.on_message(filters.command('delchnl') & filters.private & admin)
 async def del_force_sub(client: Client, message: Message):
     temp = await message.reply("<b><i>ᴡᴀɪᴛ ᴀ sᴇᴄ..</i></b>", quote=True)
     args = message.text.split(maxsplit=1)
     all_channels = await db.show_channels()
 
-    if len(args) != 2:
-        await temp.edit("<b>Usage:</b> <code>/delchnl &lt;channel_id | all&gt;</code>")
+    if len(args) < 2:
+        buttons = [[InlineKeyboardButton("ᴄʟᴏsᴇ ✖️", callback_data="close")]]
+        return await temp.edit(
+            "<b>ᴜꜱᴀɢᴇ:</b> <code>/delchnl <channel_id | all</code>",
+            reply_markup=InlineKeyboardMarkup(buttons)
+        )
 
     if args[1].lower() == "all":
         if not all_channels:
             return await temp.edit("<b>❌ ɴᴏ ғᴏʀᴄᴇ-sᴜʙ ᴄʜᴀɴɴᴇʟs ғᴏᴜɴᴅ.</b>")
         for ch_id in all_channels:
-            await db.del_channel(ch_id)
-        return await temp.edit("<b>✅ ᴀʟʟ ғᴏʀᴄᴇ-sᴜʙ ᴄʜᴀɴɴᴇʟs ʜᴀᴠᴇ ʙᴇᴇɴ ʀᴇᴍᴏᴠᴇᴅ.</b>")
+            await db.rem_channel(ch_id)
+        return await temp.edit("<b>✅ ᴀʟʟ ғᴏʀᴄᴇ-sᴜʙ ᴄʜᴀɴɴᴇʟs ʀᴇᴍᴏᴠᴇᴅ.</b>")
 
     try:
         ch_id = int(args[1])
+        if ch_id in all_channels:
+            await db.rem_channel(ch_id)
+            return await temp.edit(f"<b>✅ ᴄʜᴀɴɴᴇʟ ʀᴇᴍᴏᴠᴇᴅ:</b> <code>{ch_id}</code>")
+        else:
+            return await temp.edit(f"<b>❌ ᴄʜᴀɴɴᴇʟ ɴᴏᴛ ғᴏᴜɴᴅ:</b> <code>{ch_id}</code>")
     except ValueError:
-        return await temp.edit("<b>❌ ɪɴᴠᴀʟɪᴅ ᴄʜᴀɴɴᴇʟ ɪᴅ</b>")
+        buttons = [[InlineKeyboardButton("ᴄʟᴏsᴇ ✖️", callback_data="close")]]
+        return await temp.edit(
+            "<b>ᴜꜱᴀɢᴇ:</b> <code>/delchnl <channel_id | all</code>",
+            reply_markup=InlineKeyboardMarkup(buttons)
+    except Exception as e:
+        return await temp.edit(f"<b>❌ ᴇʀʀᴏʀ:</b> <code>{e}</code>")
 
-    if ch_id in all_channels:
-        await db.rem_channel(ch_id)
-        return await temp.edit(f"<b>✅ ᴄʜᴀɴɴᴇʟ ʀᴇᴍᴏᴠᴇᴅ:</b> <code>{ch_id}</code>")
-    else:
-        return await temp.edit(f"<b>❌ ᴄʜᴀɴɴᴇʟ ɴᴏᴛ ғᴏᴜɴᴅ ɪɴ ғᴏʀᴄᴇ-sᴜʙ ʟɪsᴛ:</b> <code>{ch_id}</code>")
-
-# View all channels
 @Bot.on_message(filters.command('listchnl') & filters.private & admin)
 async def list_force_sub_channels(client: Client, message: Message):
     temp = await message.reply("<b><i>ᴡᴀɪᴛ ᴀ sᴇᴄ..</i></b>", quote=True)
@@ -218,21 +167,12 @@ async def list_force_sub_channels(client: Client, message: Message):
     for ch_id in channels:
         try:
             chat = await client.get_chat(ch_id)
-            link = chat.invite_link or await client.export_chat_invite_link(chat.id)
+            link = await client.export_chat_invite_link(ch_id) if not chat.username else f"https://t.me/{chat.username}"
             result += f"<b>•</b> <a href='{link}'>{chat.title}</a> [<code>{ch_id}</code>]\n"
         except Exception:
             result += f"<b>•</b> <code>{ch_id}</code> — <i>ᴜɴᴀᴠᴀɪʟᴀʙʟᴇ</i>\n"
 
-    await temp.edit(result, disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Close ✖️", callback_data="close")]]))
-
-# Don't Remove Credit @CodeFlix_Bots, @rohit_1888
-# Ask Doubt on telegram @CodeflixSupport
-#
-# Copyright (C) 2025 by Codeflix-Bots@Github, < https://github.com/Codeflix-Bots >.
-#
-# This file is part of < https://github.com/Codeflix-Bots/FileStore > project,
-# and is released under the MIT License.
-# Please see < https://github.com/Codeflix-Bots/FileStore/blob/master/LICENSE >
-#
-# All rights reserved.
-#
+    buttons = [[InlineKeyboardButton("ᴄʟᴏsᴇ ✖️", callback_data="close")]]
+    await temp.edit(result, 
+                   disable_web_page_preview=True, 
+                   reply_markup=InlineKeyboardMarkup(buttons))
