43ea793ef70245333da9f49ad5f824d69dd4f836
whoami847 <mrbeastbangla069@gmail.com>
2025-05-04 16:36:34 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,8 @@
-# bot.py
-
 from aiohttp import web
 from plugins import web_server
 import asyncio
 import pyromod.listen
-from pyrogram import Client, filters  # Added filters import
+from pyrogram import Client, filters
 from pyrogram.enums import ParseMode
 import sys
 from datetime import datetime
