a5922a56edfe21ce4cd0b5e4cd5dd08c0c2dbce0
whoami847 <mrbeastbangla069@gmail.com>
2025-05-04 16:10:02 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+# bot.py
+
 from aiohttp import web
 from plugins import web_server
 import asyncio
@@ -6,14 +8,12 @@ from pyrogram import Client
 from pyrogram.enums import ParseMode
 import sys
 from datetime import datetime
-#Aɴɪᴍᴇ Lᴏʀᴅ
 from config import *
-
+from database.database import *
 
 name ="""
  **BY Aɴɪᴍᴇ Lᴏʀᴅ**
 """
-
 
 class Bot(Client):
     def __init__(self):
@@ -37,7 +37,7 @@ class Bot(Client):
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
             self.db_channel = db_channel
-            test = await self.send_message(chat_id = db_channel.id, text = "Test Message")
+            test = await self.send_message(chat_id=db_channel.id, text="Test Message")
             await test.delete()
         except Exception as e:
             self.LOGGER(__name__).warning(e)
@@ -53,21 +53,20 @@ class Bot(Client):
         self.username = usr_bot_me.username
         self.LOGGER(__name__).info(f"ʙᴏᴛ ɪꜱ ᴀʟɪᴠᴇ..! ᴍᴀᴅᴇ ʙʏ @Aɴɪᴍᴇ Lᴏʀᴅ")   
 
-        # Start Web Server
         app = web.AppRunner(await web_server())
         await app.setup()
         await web.TCPSite(app, "0.0.0.0", PORT).start()
 
-
-        try: await self.send_message(OWNER_ID, text = f"<b><blockquote> Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ by @Anime_Lord_Bot</blockquote></b>")
-        except: pass
+        try:
+            await self.send_message(OWNER_ID, text=f"<b><blockquote> Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ by @Anime_Lord_Bot</blockquote></b>")
+        except:
+            pass
 
     async def stop(self, *args):
         await super().stop()
         self.LOGGER(__name__).info("ʙᴏᴛ ꜱᴛᴏᴘᴘᴇᴅ.")
 
     def run(self):
-        """Run the bot."""
         loop = asyncio.get_event_loop()
         loop.run_until_complete(self.start())
         self.LOGGER(__name__).info("ʙᴏᴛ ɪꜱ ɴᴏᴡ ᴀʟɪᴠᴇ. ᴛʜᴀɴᴋꜱ ᴛᴏ @ᴡʜᴏ-ᴀᴍ-ɪ")
@@ -78,11 +77,20 @@ class Bot(Client):
         finally:
             loop.run_until_complete(self.stop())
 
-#
-# Copyright (C) 2025 by Codeflix-Bots@Github, < https://github.com/Codeflix-Bots >.
-#
-# This file is part of < https://github.com/Codeflix-Bots/FileStore > project,
-# and is released under the MIT License.
-# Please see < https://github.com/Codeflix-Bots/FileStore/blob/master/LICENSE >
-#
-# All rights reserved.
+# Handle admin photo uploads for setting images
+@Bot.on_message(filters.photo & filters.private & admin)
+async def handle_admin_photo(client: Client, message: Message):
+    chat_id = message.chat.id
+    state = await db.get_temp_state(chat_id)
+    if state in ["set_start", "set_help", "set_about"]:
+        type_map = {"set_start": "start", "set_help": "help", "set_about": "about"}
+        type = type_map[state]
+        file_id = message.photo.file_id
+        await db.add_image(type, file_id)
+        await message.reply_text(
+            f"New {type} image was set! Now current number of {type} images: {len(await db.get_images(type))}",
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="close")]])
+        )
+        await db.clear_temp_state(chat_id)
+    else:
+        await message.reply_text("I'm not waiting for any image.")
